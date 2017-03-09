60debbf51ab4fb240f988b521fda14885a84dede
Bowen Zhong <bz4e14@soton.ac.uk>
2017-03-09 16:43:21 +0800

    using chokidar to watch file

--- server.js
+++ server.js
@@ -1,37 +1,11 @@
 var st = require('st');
 var express = require('express');
+var chokidar = require('chokidar');
 var http = require('http');
+var fs = require('fs');
 var app = express();
 
-
-http.createServer(
-	st(process.cwd())
-).listen(1337);
-
 var mount = st({
-	path: __dirname + '/static',
-	url: '/static'
-});
-
-http.createServer(function(req, res) {
-	var stHandled = mount(req, res);
-	if (stHandled) {
-		return;
-	} else {
-		res.end('this is not a static file');
-	}
-
-}).listen(1338);
-
-// express style
-http.createServer(function(req, res) {
-  mount(req, res, function() {
-    res.end('this is not a static file')
-  })
-
-}).listen(1339);
-
-var mount2 = st({
   path: __dirname + '/static/',
   dot: true,
   // index: false,
@@ -40,23 +14,49 @@ var mount2 = st({
 });
 
 http.createServer(function(req, res) {
-    if (mount2(req, res)) {
-        return;
-    } else {
-        res.end('error');
-    }
+  if (mount(req, res)) {
+    return;
+  } else {
+    res.end('error');
+  }
 }).listen(1340);
 
-
-// express way
-// app.use(mount2);
-
-app.route('/subfolder/:file')
-.get(function(req, res, next) {
-	console.log(req);
-	mount2(req, res, next);
+fs.stat('./static/file.html', function(err, stats) {
+	console.log(stats);
 });
 
-app.listen(1341, function() {
-	console.log('Express listen on port 1341');
-});
+// fs.stat('./static/file2.html', function(err, stats) {
+// 	console.log(err);
+// 	console.log(stats);
+// });
+
+const opts = {
+  persistent: true,
+  ignoreInitial: true,
+  followSymlinks: true,
+  usePolling: true,
+  alwaysStat: false,
+  depth: undefined,
+  interval: 100,
+  ignorePermissionErrors: false,
+  atomic: true
+};
+
+const locales = chokidar.watch(path.join(__dirname, 'locale'), opts)
+const layouts = chokidar.watch(path.join(__dirname, 'layouts'), opts)
+const statics = chokidar.watch(path.join(__dirname, 'static'), opts)
+
+// var log = console.log.bind(console);
+
+// var watcher = chokidar.watch('./static/', {
+// 	ignored: /[\/\\]\./
+// });
+
+// watcher.on('change', function(path) {
+// 	log(`File ${path} has been changed`);
+// });
+
+
+
+
+
